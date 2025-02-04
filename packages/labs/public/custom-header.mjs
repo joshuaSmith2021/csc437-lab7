f5ca540100cf5f4ebba5828e8a96ff297c64bfce
Joshua Smith <joshuasmith6556@gmail.com>
2025-02-03 17:11:58 -0800

    Complete lab 9

--- packages/labs/public/custom-header.mjs
+++ packages/labs/public/custom-header.mjs
@@ -1,5 +1,7 @@
 import { attachShadow } from './utils.mjs';
 
+const CUSTOM_ELEMENT_TAG = 'custom-header'
+
 const buildHeader = (pathname) => {
   const isSubpage = pathname === '/subpage.html'
 
@@ -18,36 +20,93 @@ header {
     padding: 1rem;
     background-color: var(--color-header);
     display: flex;
-    align-items: baseline;
+    flex-direction: row;
+    justify-content: space-between;
     gap: 1rem;
+    align-items: baseline;
 }
 
-header a {
-    margin: 0 1rem;
+header > div {
+    display: flex;
+    flex-direction: column;
+}
+
+header nav {
+    display: none;
+    flex-direction: column;
+}
+
+nav div {
+    margin: 0.25rem 0;
+}
+
+button {
+    padding: 0.5rem;
+    font-size: 1.25rem;
+}
+
+@media (min-width: 700px) {
+    button {
+      display: none;
+    }
+
+    header nav {
+      display: flex !important;
+    }
+
+    header > div {
+      display: flex;
+      flex-direction: row;
+      gap: 1em;
+      align-items: stretch;
+    }
+
+    header nav {
+      flex-direction: row;
+      gap: 1rem;
+      align-items: flex-end;
+    }
 }
     </style>
     <header>
-      <h1>${isSubpage ? 'Books' : 'Joshua Smith'}</h1>
-      <nav>
-        <a href="index.html">
-          ${isSubpage ? 'Home' : '<i>Home</i>'}
-        </a>
-        <a href="subpage.html">
-          ${isSubpage ? '<i>Books</i>' : 'Books'}
-        </a>
-      </nav>
+      <div>
+        <h1>${isSubpage ? 'Books' : 'Joshua Smith'}</h1>
+        <nav>
+          <div>
+            <a href="index.html">
+              ${isSubpage ? 'Home' : '<i>Home</i>'}
+            </a>
+          </div>
+          <div>
+            <a href="subpage.html">
+              ${isSubpage ? '<i>Books</i>' : 'Books'}
+            </a>
+          </div>
+        </nav>
+      </div>
+      <button>Menu</button>
     </header>
   `)
 }
 
 const TEMPLATE = document.createElement('template')
 TEMPLATE.innerHTML = buildHeader(window.location.pathname)
-console.log(TEMPLATE.innerHTML)
 
 class CustomHeader extends HTMLElement {
   connectedCallback() {
-    attachShadow(this, TEMPLATE)
+    const shadow = attachShadow(this, TEMPLATE)
+    const nav = shadow.querySelector('nav')
+    const toggle = shadow.querySelector('button')
+    toggle.addEventListener('click', () => {
+      nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex' 
+    })
+
+    document.body.addEventListener('click', (e) => {
+      if (!document.querySelector(CUSTOM_ELEMENT_TAG).contains(e.target)) {
+        nav.style.display = 'none';
+      }
+    })
   }
 }
 
-customElements.define('custom-header', CustomHeader)
+window.customElements.define(CUSTOM_ELEMENT_TAG, CustomHeader)
