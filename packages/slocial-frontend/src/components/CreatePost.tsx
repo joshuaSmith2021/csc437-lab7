b0190c865863c8d4b2692a13ec5da6e1f33299af
Joshua Smith <joshuasmith6556@gmail.com>
2025-02-24 16:25:23 -0800

    Remove pagelaout from createpost page

--- packages/slocial-frontend/src/components/CreatePost.tsx
+++ packages/slocial-frontend/src/components/CreatePost.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import PageLayout from "./Layout";
 import { ERROR_TEXT_CLASSNAME } from "../util/classnames";
 
 export default function CreatePost() {
@@ -18,35 +17,33 @@ export default function CreatePost() {
   };
 
   return (
-    <PageLayout hideFooter>
-      <div className="w-screen flex justify-center items-center min-h-[20rem]">
-        <form className="flex flex-col gap-3">
-          <label className="block">
-            <p>Upload</p>
-            <input
-              type="file"
-              accept="image/png image/jpeg"
-              onChange={(e) => setFiles(e.currentTarget.files)}
-            />
-          </label>
-          <label className="block">
-            <p>Caption</p>
-            <input
-              type="text"
-              maxLength={300}
-              placeholder="Write something..."
-              value={caption}
-              onChange={(e) => setCaption(e.currentTarget.value)}
-            />
-          </label>
-          <button className="self-start" onClick={handleSubmit}>
-            Upload
-          </button>
-          {(error && <div className={ERROR_TEXT_CLASSNAME}>{error}</div>) || (
-            <>{/* Spacer */}</>
-          )}
-        </form>
-      </div>
-    </PageLayout>
+    <div className="w-screen flex justify-center items-center min-h-[20rem]">
+      <form className="flex flex-col gap-3">
+        <label className="block">
+          <p>Upload</p>
+          <input
+            type="file"
+            accept="image/png image/jpeg"
+            onChange={(e) => setFiles(e.currentTarget.files)}
+          />
+        </label>
+        <label className="block">
+          <p>Caption</p>
+          <input
+            type="text"
+            maxLength={300}
+            placeholder="Write something..."
+            value={caption}
+            onChange={(e) => setCaption(e.currentTarget.value)}
+          />
+        </label>
+        <button className="self-start" onClick={handleSubmit}>
+          Upload
+        </button>
+        {(error && <div className={ERROR_TEXT_CLASSNAME}>{error}</div>) || (
+          <>{/* Spacer */}</>
+        )}
+      </form>
+    </div>
   );
 }
